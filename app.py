849c8b761713a1d0dad1e05ec0735e16bccf3894
Feitosa910 <valeria.feitosa2@hotmail.com>
2024-10-01 17:24:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.write(' ')
 col1, col2 = st.columns(2)
 col3, col4 = st.columns(2)
 with col1:
-     st.image('abertas.png', caption='pt.vecteezy.com', width=590)
+     st.image('abertas.png', caption='pt.vecteezy.com', width=550)
 with col2:
      st.write('### Análise Ano a Ano')
      st.write('Olhando apenas para o número de microempresas que abriram:')
